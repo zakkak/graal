56b3c43d29e3203b6f13567a59c8e5c53f8b81d2
Galder Zamarreño <galder@redhat.com>
2020-12-07 12:53:11 +0100

    Use CSV instead

--- substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/reports/CallTreeCypher.java
+++ substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/reports/CallTreeCypher.java
@@ -6,9 +6,12 @@ import com.oracle.graal.pointsto.reports.CallTreePrinter.InvokeNode;
 import com.oracle.graal.pointsto.reports.CallTreePrinter.MethodNode;
 import com.oracle.graal.pointsto.reports.CallTreePrinter.MethodNodeReference;
 import com.oracle.graal.pointsto.reports.CallTreePrinter.Node;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -23,11 +26,6 @@ import java.util.stream.Stream;
 
 public class CallTreeCypher {
 
-    // TODO temporarily use a system property to try different values
-    private static final int BATCH_SIZE = Integer.getInteger("cypher.batch.size", 256);
-
-    private static final String METHOD_INFO_FORMAT = "name:'%n', signature:'" + CallTreePrinter.METHOD_FORMAT + "'";
-
     private static final AtomicInteger virtualNodeId = new AtomicInteger(-1);
 
     public static void print(BigBang bigbang, String path, String reportName) {
@@ -40,28 +38,13 @@ public class CallTreeCypher {
         // Set virtual node at next available method id
         virtualNodeId.set(MethodNode.methodId);
 
-        ReportUtils.report("call tree cypher in batches of " + BATCH_SIZE, path + File.separatorChar + "reports", "call_tree_" + reportName, "cypher",
-                writer -> printCypher(printer.methodToNode, writer));
+        printAll(printer.methodToNode, path, reportName);
     }
 
-    private static void printCypher(Map<AnalysisMethod, MethodNode> methodToNode, PrintWriter writer) {
-        writer.print(snippetVM());
-        printAll(methodToNode, writer);
-    }
-
-    private static String snippetVM() {
-        return ":param rows => [{_id:-1, properties:{name:'VM'}}]\n" +
-                ":begin\n" +
-                "UNWIND $rows as row\n" +
-                "CREATE (n:`UNIQUE IMPORT LABEL`{`UNIQUE IMPORT ID`: row._id})\n" +
-                "  SET n += row.properties SET n:VM;\n" +
-                ":commit\n\n";
-    }
-
-    private static void printAll(Map<AnalysisMethod, MethodNode> methodToNode, PrintWriter writer) {
-        Set<MethodNode> entryNodes = new HashSet<>();
+    private static void printAll(Map<AnalysisMethod, MethodNode> methodToNode, String path, String reportName) {
+        Set<Integer> entryPointIds = new HashSet<>();
         Set<MethodNode> nonVirtualNodes = new HashSet<>();
-        Map<String, Integer> virtualNodes = new HashMap<>();
+        Map<List<String>, Integer> virtualNodes = new HashMap<>();
 
         Map<Integer, Set<BciEndEdge>> directEdges = new HashMap<>();
         Map<Integer, Set<BciEndEdge>> virtualEdges = new HashMap<>();
@@ -70,21 +53,72 @@ public class CallTreeCypher {
         final Iterator<MethodNode> iterator = methodToNode.values().stream().filter(n -> n.isEntryPoint).iterator();
         while (iterator.hasNext()) {
             final MethodNode node = iterator.next();
-            entryNodes.add(node);
+            entryPointIds.add(node.id);
             walkNodes(node, directEdges, virtualEdges, overridenByEdges, virtualNodes, nonVirtualNodes);
         }
 
-        printNodes(entryNodes, writer);
-        printNodes(nonVirtualNodes, writer);
-        printVirtualNodes(virtualNodes, writer);
+        final String vmFileName = ReportUtils.report("call tree for vm entry point", path + File.separatorChar + "reports", "csv_call_tree_vm_" + reportName, "csv",
+                CallTreeCypher::printVMEntryPoint);
 
-        printEntryEdges(entryNodes, writer);
-        printBciEdges("DIRECT", directEdges, writer);
-        printBciEdges("VIRTUAL", virtualEdges, writer);
-        printNonBciEdges(overridenByEdges, writer);
+        final String methodsFileName = ReportUtils.report("call tree for methods", path + File.separatorChar + "reports", "csv_call_tree_methods_" + reportName, "csv",
+                writer -> printMethodNodes(methodToNode.values(), writer));
+
+        final String virtualMethodsFileName = ReportUtils.report("call tree for virtual methods", path + File.separatorChar + "reports", "csv_call_tree_virtual_methods_" + reportName, "csv",
+                writer -> printVirtualNodes(virtualNodes, writer));
+
+        final String entryPointsFileName = ReportUtils.report("call tree for entry points", path + File.separatorChar + "reports", "csv_call_tree_entry_points_" + reportName, "csv",
+                writer -> printEntryPointIds(entryPointIds, writer));
+
+        final String directEdgesFileName = ReportUtils.report("call tree for direct edges", path + File.separatorChar + "reports", "csv_call_tree_direct_edges_" + reportName, "csv",
+                writer -> printBciEdges(directEdges, writer));
+
+        final String overridenByEdgesFileName = ReportUtils.report("call tree for overriden by edges", path + File.separatorChar + "reports", "csv_call_tree_override_by_edges_" + reportName, "csv",
+                writer -> printNonBciEdges(overridenByEdges, writer));
+
+        final String virtualEdgesFileName = ReportUtils.report("call tree for virtual edges", path + File.separatorChar + "reports", "csv_call_tree_virtual_edges_" + reportName, "csv",
+                writer -> printBciEdges(virtualEdges, writer));
+
+        ReportUtils.report("call tree cypher", path + File.separatorChar + "reports", "cypher_call_tree_" + reportName, "cypher",
+                writer -> printCypher(vmFileName, methodsFileName, virtualMethodsFileName, entryPointsFileName, directEdgesFileName, overridenByEdgesFileName, virtualEdgesFileName, writer));
     }
 
-    private static void walkNodes(MethodNode methodNode, Map<Integer, Set<BciEndEdge>> directEdges, Map<Integer, Set<BciEndEdge>> virtualEdges, Map<Integer, Set<Integer>> overridenByEdges, Map<String, Integer> virtualNodes, Set<MethodNode> nonVirtualNodes) {
+    private static void printCypher(String vmFileName, String methodsFileName, String virtualMethodsFileName, String entryPointsFileName, String directEdgesFileName, String overridenByEdgesFileName, String virtualEdgesFileName, PrintWriter writer) {
+        writer.println(String.format("LOAD CSV WITH HEADERS FROM 'file:///%s' AS row", vmFileName));
+        writer.println("MERGE (v:VM {vmId: row.Id, name: row.Name});");
+
+        writer.println(String.format("LOAD CSV WITH HEADERS FROM 'file:///%s' AS row", methodsFileName));
+        writer.println("MERGE (m:Method {methodId: row.Id, name: row.Name, package: row.Package, parameters: row.Parameters, return: row.Return});");
+
+        writer.println(String.format("LOAD CSV WITH HEADERS FROM 'file:///%s' AS row", virtualMethodsFileName));
+        writer.println("MERGE (m:Method {methodId: row.Id, name: row.Name, package: row.Package, parameters: row.Parameters, return: row.Return});");
+
+        writer.println(String.format("LOAD CSV WITH HEADERS FROM 'file:///%s' AS row", entryPointsFileName));
+        writer.println("MATCH (m:Method {methodId: row.Id})");
+        writer.println("MATCH (v:VM {vmId: '0'})");
+        writer.println("MERGE (v)-[:ENTRY]->(m);");
+
+        writer.println(String.format("LOAD CSV WITH HEADERS FROM 'file:///%s' AS row", directEdgesFileName));
+        writer.println("MATCH (m1:Method {methodId: row.StartId})");
+        writer.println("MATCH (m2:Method {methodId: row.EndId})");
+        writer.println("MERGE (m1)-[:DIRECT {bci: row.BytecodeIndexes}]->(m2);");
+
+        writer.println(String.format("LOAD CSV WITH HEADERS FROM 'file:///%s' AS row", overridenByEdgesFileName));
+        writer.println("MATCH (m1:Method {methodId: row.StartId})");
+        writer.println("MATCH (m2:Method {methodId: row.EndId})");
+        writer.println("MERGE (m1)-[:OVERRIDEN_BY]->(m2);");
+
+        writer.println(String.format("LOAD CSV WITH HEADERS FROM 'file:///%s' AS row", virtualEdgesFileName));
+        writer.println("MATCH (m1:Method {methodId: row.StartId})");
+        writer.println("MATCH (m2:Method {methodId: row.EndId})");
+        writer.println("MERGE (m1)-[:VIRTUAL {bci: row.BytecodeIndexes}]->(m2);");
+    }
+
+    private static void printVMEntryPoint(PrintWriter writer) {
+        writer.println(convertToCSV("Id", "Name"));
+        writer.println(convertToCSV("0", "VM"));
+    }
+
+    private static void walkNodes(MethodNode methodNode, Map<Integer, Set<BciEndEdge>> directEdges, Map<Integer, Set<BciEndEdge>> virtualEdges, Map<Integer, Set<Integer>> overridenByEdges, Map<List<String>, Integer> virtualNodes, Set<MethodNode> nonVirtualNodes) {
         for (InvokeNode invoke : methodNode.invokes) {
             if (invoke.isDirectInvoke) {
                 if (invoke.callees.size() > 0) {
@@ -107,30 +141,21 @@ public class CallTreeCypher {
         }
     }
 
-    private static void printNodes(Set<MethodNode> nodes, PrintWriter writer) {
-        final Collection<List<MethodNode>> batches = batched(nodes);
-        final String snippet = snippetMethod();
-        final String script = batches.stream()
-                .map(methodBatch -> methodBatch.stream()
-                        .map(method -> String.format("{_id:%d, %s}", method.id, asProperties(method.method.format(METHOD_INFO_FORMAT))))
-                        .collect(Collectors.joining(", ", ":param rows => [", "]"))
-                )
-                .collect(Collectors.joining(snippet, "", snippet));
-
-        writer.print(script);
+    private static void printMethodNodes(Collection<MethodNode> methods, PrintWriter writer) {
+        writer.println(convertToCSV("Id", "Name", "Package", "Parameters", "Return"));
+        methods.stream()
+                .map(CallTreeCypher::methodNodeInfo)
+                .map(CallTreeCypher::convertToCSV)
+                .forEach(writer::println);
     }
 
-    private static String snippetMethod() {
-        return "\n\n:begin\n" +
-                "UNWIND $rows as row\n" +
-                "CREATE (n:`UNIQUE IMPORT LABEL`{`UNIQUE IMPORT ID`: row._id})\n" +
-                "  SET n += row.properties SET n:Method;\n" +
-                ":commit\n\n";
+    private static List<String> methodNodeInfo(MethodNode method) {
+        return resolvedJavaMethodInfo(method.id, method.method);
     }
 
-    private static int addVirtualNode(InvokeNode node, Map<String, Integer> virtualNodes) {
-        final String methodInfo = node.targetMethod.format(METHOD_INFO_FORMAT);
-        return virtualNodes.computeIfAbsent(methodInfo, k -> CallTreeCypher.virtualNodeId.getAndIncrement());
+    private static int addVirtualNode(InvokeNode node, Map<List<String>, Integer> virtualNodes) {
+        final List<String> virtualMethodInfo = virtualMethodInfo(node.targetMethod);
+        return virtualNodes.computeIfAbsent(virtualMethodInfo, k -> CallTreeCypher.virtualNodeId.getAndIncrement());
     }
 
     private static void addVirtualMethodEdge(int startId, InvokeNode invoke, int endId, Map<Integer, Set<BciEndEdge>> edges) {
@@ -153,16 +178,27 @@ public class CallTreeCypher {
                 .collect(Collectors.toList());
     }
 
-    private static void printVirtualNodes(Map<String, Integer> virtualNodes, PrintWriter writer) {
-        final Collection<List<Map.Entry<String, Integer>>> virtualNodesBatches = batched(virtualNodes.entrySet());
-        final String snippet = snippetMethod();
-        final String script = virtualNodesBatches.stream()
-                .map(virtualNodesBatch -> virtualNodesBatch.stream()
-                        .map(virtualNode -> String.format("{_id:%d, %s}", virtualNode.getValue(), asProperties(virtualNode.getKey())))
-                        .collect(Collectors.joining(", ", ":param rows => [", "]"))
-                )
-                .collect(Collectors.joining(snippet, "", snippet));
-        writer.print(script);
+    private static void printVirtualNodes(Map<List<String>, Integer> virtualNodes, PrintWriter writer) {
+        writer.println(convertToCSV("Id", "Name", "Package", "Parameters", "Return"));
+        virtualNodes.entrySet().stream()
+                .map(CallTreeCypher::virtualMethodAndIdInfo)
+                .map(CallTreeCypher::convertToCSV)
+                .forEach(writer::println);
+    }
+
+    private static List<String> virtualMethodAndIdInfo(Map.Entry<List<String>, Integer> entry) {
+        final List<String> methodInfo = entry.getKey();
+        final List<String> result = new ArrayList<>(methodInfo.size() + 1);
+        result.add(String.valueOf(entry.getValue()));
+        for (int i = 1; i < methodInfo.size(); i++) {
+            result.add(i, methodInfo.get(i));
+        }
+        return result;
+    }
+
+    private static void printEntryPointIds(Set<Integer> entryPoints, PrintWriter writer) {
+        writer.println(convertToCSV("Id"));
+        entryPoints.forEach(writer::println);
     }
 
     private static void addOverridenByEdge(int nodeId, Node calleeNode, Map<Integer, Set<Integer>> edges, Set<MethodNode> nodes) {
@@ -174,47 +210,15 @@ public class CallTreeCypher {
         nodeEdges.add(methodNode.id);
     }
 
-    private static void printEntryEdges(Set<MethodNode> edges, PrintWriter writer) {
-        final Collection<List<MethodNode>> batchedEdges = batched(edges);
-
-        final String snippet = snippetNonBciEdge("ENTRY");
-        final String script = batchedEdges.stream()
-                .map(edgesBatch -> edgesBatch.stream()
-                        .map(edge -> String.format("{start: {_id:%d}, end: {_id:%d}}", -1, edge.id))
-                        .collect(Collectors.joining(", ", ":param rows => [", "]"))
-                )
-                .collect(Collectors.joining(snippet, "", snippet));
-
-        writer.print(script);
-    }
-
-    private static String snippetNonBciEdge(String edgeName) {
-        return "\n\n:begin\n" +
-                "UNWIND $rows as row\n" +
-                "MATCH (start:`UNIQUE IMPORT LABEL`\n" +
-                "               {`UNIQUE IMPORT ID`: row.start._id})\n" +
-                "MATCH (end:`UNIQUE IMPORT LABEL`\n" +
-                "               {`UNIQUE IMPORT ID`: row.end._id})\n" +
-                "CREATE (start)-[r:" + edgeName + "]->(end);\n" +
-                ":commit\n\n";
-    }
-
-    private static void printBciEdges(String edgeName, Map<Integer, Set<BciEndEdge>> edges, PrintWriter writer) {
+    private static void printBciEdges(Map<Integer, Set<BciEndEdge>> edges, PrintWriter writer) {
         final Set<BciEdge> idEdges = edges.entrySet().stream()
                 .flatMap(entry -> entry.getValue().stream().map(endId -> new BciEdge(entry.getKey(), endId)))
                 .collect(Collectors.toSet());
 
-        final Collection<List<BciEdge>> batchedEdges = batched(idEdges);
-
-        final String unwindEdge = snippetBciEdge(edgeName);
-        final String script = batchedEdges.stream()
-                .map(edgesBatch -> edgesBatch.stream()
-                        .map(edge -> String.format("{start: {_id:%d}, end: {_id:%d}, bci: '%s'}", edge.startId, edge.endEdge.id, showBytecodeIndexes(edge.endEdge.bytecodeIndexes)))
-                        .collect(Collectors.joining(", ", ":param rows => [", "]"))
-                )
-                .collect(Collectors.joining(unwindEdge, "", unwindEdge));
-
-        writer.print(script);
+        writer.println(convertToCSV("StartId", "EndId", "BytecodeIndexes"));
+        idEdges.stream()
+                .map(edge -> convertToCSV(String.valueOf(edge.startId), String.valueOf(edge.endEdge.id), showBytecodeIndexes(edge.endEdge.bytecodeIndexes)))
+                .forEach(writer::println);
     }
 
     private static String showBytecodeIndexes(List<Integer> bytecodeIndexes) {
@@ -223,44 +227,45 @@ public class CallTreeCypher {
                 .collect(Collectors.joining("->"));
     }
 
-    private static String snippetBciEdge(String edgeName) {
-        return "\n\n:begin\n" +
-                "UNWIND $rows as row\n" +
-                "MATCH (start:`UNIQUE IMPORT LABEL`\n" +
-                "               {`UNIQUE IMPORT ID`: row.start._id})\n" +
-                "MATCH (end:`UNIQUE IMPORT LABEL`\n" +
-                "               {`UNIQUE IMPORT ID`: row.end._id})\n" +
-                "CREATE (start)-[r:" + edgeName + " { bci: row.bci }]->(end);\n" +
-                ":commit\n\n";
-    }
-
     private static void printNonBciEdges(Map<Integer, Set<Integer>> edges, PrintWriter writer) {
         final Set<NonBciEdge> idEdges = edges.entrySet().stream()
                 .flatMap(entry -> entry.getValue().stream().map(endId -> new NonBciEdge(entry.getKey(), endId)))
                 .collect(Collectors.toSet());
 
-        final Collection<List<NonBciEdge>> batchedEdges = batched(idEdges);
-
-        final String unwindEdge = snippetNonBciEdge("OVERRIDDEN_BY");
-        final String script = batchedEdges.stream()
-                .map(edgesBatch -> edgesBatch.stream()
-                        .map(edge -> String.format("{start: {_id:%d}, end: {_id:%d}}", edge.startId, edge.endId))
-                        .collect(Collectors.joining(", ", ":param rows => [", "]"))
-                )
-                .collect(Collectors.joining(unwindEdge, "", unwindEdge));
-
-        writer.print(script);
+        writer.println(convertToCSV("StartId", "EndId"));
+        idEdges.stream()
+                .map(edge -> convertToCSV(String.valueOf(edge.startId), String.valueOf(edge.endId)))
+                .forEach(writer::println);
     }
 
-    private static String asProperties(String properties) {
-        return "properties:{" + properties + "}";
+    private static List<String> virtualMethodInfo(AnalysisMethod method) {
+        return resolvedJavaMethodInfo(null, method);
     }
 
-    private static <E> Collection<List<E>> batched(Collection<E> methods) {
-        final AtomicInteger counter = new AtomicInteger();
-        return methods.stream()
-                .collect(Collectors.groupingBy(m -> counter.getAndIncrement() / BATCH_SIZE))
-                .values();
+    private static List<String> resolvedJavaMethodInfo(Integer id, ResolvedJavaMethod method) {
+        // TODO method parameter types are opaque, but could in the future be split out and link together
+        //      e.g. each method could BELONG to a type, and a method could have PARAMETER relationships with N types
+        //      see https://neo4j.com/developer/guide-import-csv/#_converting_data_values_with_load_csv for examples
+        final String parameters =
+                method.getSignature().getParameterCount(false) > 0
+                        ? method.format("%P").replace(",", "")
+                        : "empty";
+
+        return Arrays.asList(
+                id == null ? null : Integer.toString(id),
+                method.getName(),
+                method.getDeclaringClass().toJavaName(true),
+                parameters,
+                method.getSignature().getReturnType(null).toJavaName(true)
+        );
+    }
+
+    private static String convertToCSV(String... data) {
+        return String.join(",", data);
+    }
+
+    private static String convertToCSV(List<String> data) {
+        return String.join(",", data);
     }
 
     private static final class NonBciEdge {
--- substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/reports/ReportUtils.java
+++ substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/reports/ReportUtils.java
@@ -72,13 +72,15 @@ public class ReportUtils {
      * @param name the name of the report
      * @param extension the extension of the report
      * @param reporter a consumer that writes to a PrintWriter
+     * @return file name for the report
      */
-    public static void report(String description, String path, String name, String extension, Consumer<PrintWriter> reporter) {
+    public static String report(String description, String path, String name, String extension, Consumer<PrintWriter> reporter) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
         String timeStamp = LocalDateTime.now().format(formatter);
         Path reportDir = Paths.get(path);
         String fileName = name + "_" + timeStamp + "." + extension;
         reportImpl(description, reportDir, fileName, reporter);
+        return fileName;
     }
 
     /**
